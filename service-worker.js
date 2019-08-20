2002afe4b3fa2ddfdc3a20de869aa7b3c0bffa03
Shaimoogle <Shaimoogle>
2019-08-20 03:12:36 +0200

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,92 @@
+var CACHE_NAME = 'ggapp-cache-v1';
+var urlsToCache = [
+  '/',
+  '/offline.html',
+  '/styles/main.css',
+  '/script/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  if (event.request.mode !== 'navigate') {
+    // Not a page navigation, bail.
+    return;
+  }
+    event.respondWith(
+      fetch(event.request)
+      .catch(() => {
+        return caches.open(CACHE_NAME)
+            .then((cache) => {
+              return cache.match('/offline.html');
+            });
+      }),
+
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+  
+          return fetch(event.request).then(
+            function(response) {
+              // Check if we received a valid response
+              if(!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+              }
+  
+              // IMPORTANT: Clone the response. A response is a stream
+              // and because we want the browser to consume the response
+              // as well as the cache consuming the response, we need
+              // to clone it so we have two streams.
+              var responseToCache = response.clone();
+  
+              caches.open(CACHE_NAME)
+                .then(function(cache) {
+                  cache.put(event.request, responseToCache);
+                });
+  
+              return response;
+            }
+          );
+        }),
+      );
+  });
+
+  self.addEventListener('activate', function(event) {
+    var cacheWhitelist = ['ggapp-cache-v1'];
+
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+
+  // CODELAB: Remove previous cached data from disk.
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  });
