40755c5a9e28b70fba4102957e7f977a0ba30235
Shaimoogle <Shaimoogle>
2019-08-21 18:17:52 +0200

    Content throw #1

--- scripts/main.js
+++ scripts/main.js
@@ -10,4 +10,13 @@ $(document).ready(function () {
         $("#blackveil").removeClass('active');
     });
 
+    $('#chat_button').on('click', function() {
+        $("#content").load('https://shaimoogle.github.io/GGApp/chat.html');
+    })
+
+    $('#invoices_button').on('click', function() {
+        $("#content").load('https://shaimoogle.github.io/GGApp/invoices.html');
+    })
+
+
 });
