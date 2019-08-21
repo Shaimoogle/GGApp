31d32ae5b586a1f52c2a7fedd36c67bbb9543300
Shaimoogle <Shaimoogle>
2019-08-21 18:20:09 +0200

    Content throw #2

--- scripts/main.js
+++ scripts/main.js
@@ -12,10 +12,14 @@ $(document).ready(function () {
 
     $('#chat_button').on('click', function() {
         $("#content").load('https://shaimoogle.github.io/GGApp/chat.html');
+        $("#sidebar").removeClass('active');
+        $("#blackveil").removeClass('active');
     })
 
     $('#invoices_button').on('click', function() {
         $("#content").load('https://shaimoogle.github.io/GGApp/invoices.html');
+        $("#sidebar").removeClass('active');
+        $("#blackveil").removeClass('active');
     })
 
 
